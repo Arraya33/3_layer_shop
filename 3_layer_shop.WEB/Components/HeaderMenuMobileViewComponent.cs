03ca7cf84bff89f6aab69f731896fe6a8ef9936e
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-14 21:29:14 +0300

    вынес мобильное меню шапки в компонент HeaderMenuMobile

---
+++ 3_layer_shop.WEB/Components/HeaderMenuMobileViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3_layer_shop.WEB.Components
+{
+    public class HeaderMenuMobileViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

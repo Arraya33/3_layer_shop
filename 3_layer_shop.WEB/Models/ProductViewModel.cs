3abcc571a4809405448bc5f36e50b6a3be2b7c6f
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-17 15:52:00 +0300

    добавил модель представления для товара

---
+++ 3_layer_shop.WEB/Models/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3_layer_shop.WEB.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int DiscountPrice { get; set; }
+        public string IntroText { get; set; }
+        public string Description { get; set; }
+        public string MainImage { get; set; }
+        public List<string> Images { get; set; }
+        public int Quantity { get; set; }
+    }
+}

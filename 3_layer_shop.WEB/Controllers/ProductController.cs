cf2c9acb1f0df4f6cfbf154246bed589264306d8
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-20 15:40:57 +0300

    передал в представление страницы категории фиктивные товары

--- 3_layer_shop.WEB/Controllers/ProductController.cs
+++ 3_layer_shop.WEB/Controllers/ProductController.cs
@@ -25,9 +25,33 @@ namespace _3_layer_shop.WEB.Controllers
             return View();
         }
 
-        public ActionResult List(string categoryAlias)
+        public ActionResult List(string categoryAlias, int page = 1)
         {
-            return View();
+            List<ProductViewModel> products = new List<ProductViewModel>();
+            products.Add(new ProductViewModel { MainImage = "/images/product_1.jpg", Name = "Product1", Alias = "product_1", Price = 123, DiscountPrice = 55 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_2.jpg", Name = "Product2", Alias = "product_2", Price = 234, DiscountPrice = 55 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_3.jpg", Name = "Product3", Alias = "product_3", Price = 6434 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_4.jpg", Name = "Product4", Alias = "product_4", Price = 23 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_5.jpg", Name = "Product5", Alias = "product_5", Price = 433, DiscountPrice = 55 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_6.jpg", Name = "Product6", Alias = "product_6", Price = 655 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_7.jpg", Name = "Product7", Alias = "product_7", Price = 234, DiscountPrice = 55 });
+            products.Add(new ProductViewModel { MainImage = "/images/product_8.jpg", Name = "Product8", Alias = "product_8", Price = 111 });
+
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = _pageSize,
+                TotalItems = products.Count
+            };
+
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = products.Skip((page - 1) * _pageSize).Take(_pageSize),
+                PagingInfo = pagingInfo,
+                Name = "Категория товаров"
+            };
+
+            return View(model);
         }
 
         public ActionResult DiscountList(int page = 1)

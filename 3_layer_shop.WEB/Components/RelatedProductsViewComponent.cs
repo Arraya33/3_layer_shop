6b6e3fe7ef1338a591ec4eef4dae3c739eb72fbf
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-21 21:24:06 +0300

    в компоненте "RelatedProducts" заменил статические данные динамическими

--- 3_layer_shop.WEB/Components/RelatedProductsViewComponent.cs
+++ 3_layer_shop.WEB/Components/RelatedProductsViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3_layer_shop.WEB.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,9 @@ namespace _3_layer_shop.WEB.Components
 {
     public class RelatedProductsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(IEnumerable<ProductViewModel> products)
         {
-            return View();
+            return View(products);
         }
     }
 }

ab32bd29ba7c7f96bcff9a6927c32e1a1fdbe37b
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-25 17:16:20 +0300

    заменил фиктивные данные контроллера для страницы категорий товара данными из сервиса продуктов

--- 3_layer_shop.WEB/Controllers/ProductController.cs
+++ 3_layer_shop.WEB/Controllers/ProductController.cs
@@ -28,8 +28,8 @@ namespace _3_layer_shop.WEB.Controllers
         public ActionResult Product(string productAlias)
         {
             ProductDTO productDTO = _productService.GetProduct(productAlias);
-            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<ProductDTO, ProductViewModel>()).CreateMapper();
-            var model = mapper.Map<ProductViewModel>(productDTO);
+            IMapper mapper = new MapperConfiguration(cfg => cfg.CreateMap<ProductDTO, ProductViewModel>()).CreateMapper();
+            ProductViewModel model = mapper.Map<ProductViewModel>(productDTO);
 
             ViewBag.Title = model.Name;
 
@@ -38,31 +38,25 @@ namespace _3_layer_shop.WEB.Controllers
 
         public ActionResult List(string categoryAlias, int page = 1)
         {
-            List<ProductViewModel> products = new List<ProductViewModel>();
-            products.Add(new ProductViewModel { MainImage = "/images/product_1.jpg", Name = "Product1", Alias = "product_1", Price = 123, DiscountPrice = 55 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_2.jpg", Name = "Product2", Alias = "product_2", Price = 234, DiscountPrice = 55 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_3.jpg", Name = "Product3", Alias = "product_3", Price = 6434 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_4.jpg", Name = "Product4", Alias = "product_4", Price = 23 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_5.jpg", Name = "Product5", Alias = "product_5", Price = 433, DiscountPrice = 55 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_6.jpg", Name = "Product6", Alias = "product_6", Price = 655 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_7.jpg", Name = "Product7", Alias = "product_7", Price = 234, DiscountPrice = 55 });
-            products.Add(new ProductViewModel { MainImage = "/images/product_8.jpg", Name = "Product8", Alias = "product_8", Price = 111 });
+            ProductCategoryDTO productCategoryDTO = _productService.GetProductCategory(categoryAlias, page, _pageSize);
+
+            IMapper mapper = new MapperConfiguration(cfg => 
+            { 
+                cfg.CreateMap<ProductCategoryDTO, ProductListViewModel>(); 
+                cfg.CreateMap<ProductDTO, ProductViewModel>(); 
+            }).CreateMapper();
+            ProductListViewModel model = mapper.Map<ProductListViewModel>(productCategoryDTO);
 
             PagingInfo pagingInfo = new PagingInfo
             {
                 CurrentPage = page,
                 ItemsPerPage = _pageSize,
-                TotalItems = products.Count
+                TotalItems = productCategoryDTO.TotalItems
             };
 
-            ProductListViewModel model = new ProductListViewModel
-            {
-                Products = products.Skip((page - 1) * _pageSize).Take(_pageSize),
-                PagingInfo = pagingInfo,
-                Name = "Категория товаров"
-            };
+            model.PagingInfo = pagingInfo;
 
-            ViewBag.Title = "Категория товаров";
+            ViewBag.Title = model.Name;
 
             return View(model);
         }

86eddf6090857ec424acbec438c924b186801364
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-25 17:14:01 +0300

    добавил DTO для категорий товаров

---
+++ 3_layer_shop.BLL/DTO/ProductCategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_layer_shop.BLL.DTO
+{
+    public class ProductCategoryDTO
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ProductDTO> Products { get; set; }
+        public int TotalItems { get; set; }
+    }
+}

56d4087def576ab4859fe22573a5caf774d1c010
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-25 12:37:21 +0300

    переделал шаблон HomeSlider с статических данных на динамические

--- 3_layer_shop.WEB/Components/HomeSliderViewComponent.cs
+++ 3_layer_shop.WEB/Components/HomeSliderViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3_layer_shop.WEB.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,33 @@ namespace _3_layer_shop.WEB.Components
     {
         public IViewComponentResult Invoke()
         {
-            return View();
+            List<BannerViewModel> model = new List<BannerViewModel>();
+
+            model.Add(new BannerViewModel 
+            { 
+                Image = "/images/home_slider_1.jpg",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Text text text",
+                Link = "https://google.com", 
+                Title = "Banner 1" 
+            });
+
+            model.Add(new BannerViewModel
+            {
+                Image = "/images/home_slider_1.jpg",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Text text text",
+                Link = "https://google.com",
+                Title = "Banner 2"
+            });
+
+            model.Add(new BannerViewModel
+            {
+                Image = "/images/home_slider_1.jpg",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Text text text",
+                Link = "https://google.com",
+                Title = "Banner 3"
+            });
+
+            return View(model);
         }
     }
 }

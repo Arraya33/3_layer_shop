974b2cb64a072d64d65114b66ecf9c6b39d44202
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-25 14:21:16 +0300

    добавил DTO для товаров

---
+++ 3_layer_shop.BLL/DTO/ProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_layer_shop.BLL.DTO
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int DiscountPrice { get; set; }
+        public string IntroText { get; set; }
+        public string Description { get; set; }
+        public string MainImage { get; set; }
+        public IEnumerable<string> Images { get; set; }
+        public int Quantity { get; set; }
+        public IEnumerable<ProductDTO> RelatedProducts { get; set; }
+    }
+}

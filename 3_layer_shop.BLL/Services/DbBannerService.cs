aac45ebf17065274807a4e9cbffb790eaa88ef08
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-26 09:40:01 +0300

    добавил интерфейс и реализацию сервиса баннеров для работы через БД

---
+++ 3_layer_shop.BLL/Services/DbBannerService.cs
@@ -0,0 +1,12 @@
+﻿using _3_layer_shop.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_layer_shop.BLL.Services
+{
+    public class DbBannerService : IBannerService
+    {
+
+    }
+}

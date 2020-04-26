20a6dc6498e9eb92a46e49594f1cbd0b577187a0
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-26 11:14:12 +0300

    изменил модель со списка баннеров на BannerGroup

---
+++ 3_layer_shop.WEB/Models/ViewModels/BannerGroupViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3_layer_shop.WEB.Models.ViewModels
+{
+    public class BannerGroupViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<BannerViewModel> Banners { get; set; }
+    }
+}

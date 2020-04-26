eb2646562c6f0d8c0113fdd06f9644178cb9c458
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-26 09:38:16 +0300

    добавил сервис информационных страниц для работы с БД и заполнил метод для получения списка страниц фиктивными данными

---
+++ 3_layer_shop.BLL/Services/DbInformationService.cs
@@ -0,0 +1,22 @@
+﻿using _3_layer_shop.BLL.DTO;
+using _3_layer_shop.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_layer_shop.BLL.Services
+{
+    public class DbInformationService : IInformationService
+    {
+        public IEnumerable<InformationDTO> GetInformationList()
+        {
+            IEnumerable<InformationDTO> informationList = new List<InformationDTO>
+            {
+                new InformationDTO { Alias = "article_1", Name = "article 1" },
+                new InformationDTO { Alias = "article_2", Name = "article 2" },
+                new InformationDTO { Alias = "article_3", Name = "article 3" }
+            };
+            return informationList;
+        }
+    }
+}

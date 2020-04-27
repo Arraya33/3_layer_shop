c356fa37b832d0028a00e945509b37c82e202db2
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-27 10:18:19 +0300

    добавил DTO для главной страницы

---
+++ 3_layer_shop.BLL/DTO/HomePageDTO.cs
@@ -0,0 +1,12 @@
+﻿using _3_layer_shop.BLL.DTO.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_layer_shop.BLL.DTO
+{
+    public class HomePageDTO : PageDTO
+    {
+        public IEnumerable<ProductPageDTO> Products { get; set; }
+    }
+}

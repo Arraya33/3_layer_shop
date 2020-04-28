12f953d80acea664e9f3dfe9bdacba5122112855
Alexander Lugovoy <alexlug33@gmail.com>
2020-04-28 14:00:53 +0300

    добавил конфигурации Fluent Api для работы с Entity Framework

---
+++ 3_layer_shop.DAL/EF/Configuration/ProductToProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using _3_layer_shop.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_layer_shop.DAL.EF.Configuration
+{
+    public class ProductToProductConfiguration : IEntityTypeConfiguration<ProductToProduct>
+    {
+        public void Configure(EntityTypeBuilder<ProductToProduct> builder)
+        {
+            builder.ToTable("ProductsToProducts").HasOne(ptc => ptc.ProductParent)
+                .WithMany(product => product.ProductToProductsChilds).HasForeignKey(ptc => ptc.ProductParentId).OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(ptc => ptc.ProductChild).WithMany(product => product.ProductToProductsParents).HasForeignKey(ptc => ptc.ProductChildId);
+            builder.HasKey(ptc => new { ptc.ProductChildId, ptc.ProductParentId });
+        }
+    }
+}

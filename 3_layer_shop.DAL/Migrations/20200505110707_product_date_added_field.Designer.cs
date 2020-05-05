38168abb3fbe9183da554b99f91901ecf565eba3
Alexander Lugovoy <alexlug33@gmail.com>
2020-05-05 14:08:47 +0300

    добавил миграцию, добавляющую поле с датой добавления товара

--- 3_layer_shop.DAL/Migrations/20200505110707_product_date_added_field.Designer.cs
+++ 3_layer_shop.DAL/Migrations/20200505110707_product_date_added_field.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _3_layer_shop.DAL.EF;
 
 namespace _3_layer_shop.DAL.Migrations
 {
     [DbContext(typeof(SiteDbContext))]
-    partial class SiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200505110707_product_date_added_field")]
+    partial class product_date_added_field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

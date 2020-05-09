b519d68b0182c630ccd90247b05342fb976a9e0f
Alexander Lugovoy <alexlug33@gmail.com>
2020-05-09 20:03:55 +0300

    добавил модели для корзины на слое PL

--- 3_layer_shop.WEB/Components/ShoppingCartViewComponent.cs
+++ 3_layer_shop.WEB/Components/ShoppingCartViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3_layer_shop.WEB.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,13 @@ namespace _3_layer_shop.WEB.Components
 {
     public class ShoppingCartViewComponent : ViewComponent
     {
+        private CartViewModel _cart;
+
+        public ShoppingCartViewComponent(CartViewModel cart)
+        {
+            _cart = cart;
+        }
+
         public IViewComponentResult Invoke()
         {
             return View();
---
+++ 3_layer_shop.WEB/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3_layer_shop.WEB.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<CartLine> CartLines { get; set; }
+    }
+}

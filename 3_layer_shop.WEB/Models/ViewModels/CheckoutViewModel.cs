c06afb2f9754c8e5b43d6dd243998edbf4aa21a1
Alexander Lugovoy <alexlug33@gmail.com>
2020-05-18 11:08:22 +0300

    перевел шаблоны страниц на русский, изменил демонстрационные сущности, добавляемые в БД на более читаемые.

--- 3_layer_shop.WEB/Models/ViewModels/CheckoutViewModel.cs
+++ 3_layer_shop.WEB/Models/ViewModels/CheckoutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,20 +10,26 @@ namespace _3_layer_shop.WEB.Models.ViewModels
     public class CheckoutViewModel
     {
         public IList<CartLine> CartLines { get; set; }
+
         public int TotalValue { get; set; }
 
-        [Required(ErrorMessage = "Please enter a first name")]
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя*")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter a last name")]
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия*")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter a address")]
+        [Required(ErrorMessage = "Введите адрес")]
+        [Display(Name = "Адрес*")]
         public string Address { get; set; }
 
+        [Display(Name = "Телефон")]
         public string Phone { get; set; }
 
-        [Required(ErrorMessage = "Please enter a email")]
+        [Required(ErrorMessage = "Введите e-mail")]
+        [Display(Name = "E-mail*")]
         public string Email { get; set; }
     }
 }
